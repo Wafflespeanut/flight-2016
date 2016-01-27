784ccb1514be048a49cee23a6cafddb4481527e4
Ravi Shankar <wafflespeanut@gmail.com>
2016-01-27 12:04:56 +0530

    Breaking change for clock!

--- javascripts/clock.js
+++ javascripts/clock.js
@@ -15,39 +15,64 @@ function Timer(callback, delay) {
     this.resume();
 }
 
-function distribute_points() {
+function get_time_from_date(date) {
+    var mins = date.getMinutes(), hours = date.getHours();
+    var suffix = (hours - 12 > 0) ? ' PM' : ' AM'
+    return hours % 12 + ':' + ('0' + mins).slice(-2) + suffix;
+}
+
+function rotate_hand_to(point) {
+    document.getElementById('display-event-title').innerHTML = point.event_title;
+    document.getElementById('display-event-time').innerHTML = point.event_time;
+    document.getElementById('hand').style.transform = 'rotate(' + point.angle + 'deg)';
+}
+
+function distribute_points() {      // assumes that all events begin & end in 12 hours!
     var points = document.querySelectorAll('.point');
     var num_points = points.length;
+    var event_times = document.querySelectorAll('.event-time');
+    var event_titles = document.querySelectorAll('.event-title');
+
+    if (num_points != event_times.length || num_points != event_titles.length) {
+        throw "Exception: Unequal numbers of events & times! Check 'schedule.html'!"
+    }
+
     for (var i = 0; i < num_points; i++) {
-        var x = 200 * Math.cos(2 * Math.PI * i / num_points);
-        var y = 200 * Math.sin(2 * Math.PI * i / num_points);
+        var date = new Date(event_times[i].firstChild.nodeValue);
+        var angle = (date.getHours() - 3) * 30 + date.getMinutes() / 2;
+        var x = 200 * Math.cos(Math.PI * angle / 180);
+        var y = 200 * Math.sin(Math.PI * angle / 180);
         points[i].style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-    } return num_points;
+        points[i].angle = angle;
+        points[i].event_time = get_time_from_date(date);
+        points[i].event_title = event_titles[i].firstChild.nodeValue;
+    } rotate_hand_to(points[0]);
 }
 
 function move_hand_to(e) {
-    window.timer.pause();
+    window.clock_timer.pause();
     var hand = document.getElementById('hand');
     var points = document.querySelectorAll('.point');
-    var num_points = points.length;
 
-    for (var i = 0; i < num_points; i++) {
+    for (var i = 0; i < points.length; i++) {
         if (points[i] == e) {
-            hand.idx += i - hand.idx % num_points;
-            hand.style.transform = 'rotate(' + 2 * Math.PI * hand.idx / num_points + 'rad)';
+            rotate_hand_to(points[i]);
+            hand.idx = i;
+            break;
         }
-    }
-
-    hand.idx += 1;
-    window.timer.resume();
+    } window.clock_timer.resume();
 }
 
 window.onload = function() {
-    var num_points = distribute_points();
+    distribute_points();
+    var points = document.querySelectorAll('.point');
     var hand = document.getElementById('hand');
     hand.idx = 1;
-    window.timer = new Timer(function() {       // put it inside the window so that we can access later
-        hand.style.transform = 'rotate(' + 2 * Math.PI * hand.idx / num_points + 'rad)';
+    window.clock_timer = new Timer(function() {       // put it inside the window so that we can access later
+        rotate_hand_to(points[hand.idx]);
         hand.idx += 1;
+        if (hand.idx == points.length) {
+            hand.idx = 0;
+        }
     }, 2500);
 }
