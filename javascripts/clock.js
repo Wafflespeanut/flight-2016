d739de7e56b42566fde8a9e83aeb9bb4e5a809dd
Ravi Shankar <wafflespeanut@gmail.com>
2016-01-30 21:27:40 +0530

    Clock now supports multiple events in multiple dates!

--- javascripts/clock.js
+++ javascripts/clock.js
@@ -1,18 +1,95 @@
+// Stuff that depend on the values set in CSS
+const RADIUS = 200;
+const POINT_OPACITY_TIMEOUT = 1000;
+
 function Timer(callback, delay) {
-    var timer_id, start;
+    var timer_id;
 
     this.pause = function() {
         clearInterval(timer_id);
     };
 
     this.resume = function() {
-        start = new Date();
         if (timer_id) {
             clearInterval(timer_id);
         } timer_id = setInterval(callback, delay);
     };
 
     this.resume();
+}
+
+function EventDistributor() {       // assumes that all events begin & end in 12 hours!
+    var points = document.querySelectorAll('.point');
+    var event_times = document.querySelectorAll('.event-time');
+    var event_titles = document.querySelectorAll('.event-title');
+    var event_box = document.getElementById('day-box');
+    var point_dates_obj = {}, days = [], num_points = points.length, current = -1;
+
+    if (num_points != event_times.length || num_points != event_titles.length) {
+        throw "Exception: Unequal numbers of events & times! Check 'schedule.html'!"
+    }
+
+    for (i = 0; i < num_points; i++) {
+        var raw_date = event_times[i].firstChild.nodeValue;
+        var date = new Date(raw_date);
+        var key = date.getFullYear() + ('0' + date.getMonth()).slice(-2) + date.getDate();
+
+        if (point_dates_obj[key] == undefined) {
+            point_dates_obj[key] = [];
+        }
+
+        var angle = (date.getHours() - 3) * 30 + date.getMinutes() / 2;
+        var x = RADIUS * Math.cos(Math.PI * angle / 180);
+        var y = RADIUS * Math.sin(Math.PI * angle / 180);
+        points[i].style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+        points[i].angle = angle;
+        points[i].event_time = get_time_from_date(date);
+        points[i].event_title = event_titles[i].firstChild.nodeValue;
+        if (points[i].children.length == 3) {
+            // blindly assumes that there's an "end time" for the event, and that the first child contains the time
+            end_time = new Date(points[i].querySelectorAll('.event-end')[0].firstChild.nodeValue);
+            points[i].end_angle = (end_time.getHours() - 3) * 30 + end_time.getMinutes() / 2;
+        }
+
+        point_dates_obj[key].push(i);  // remember the point corresponding to a day
+        point_dates_obj[key].display_format = raw_date.split(' ').splice(0, 2).join(' ');
+        if (!days.includes(key)) {
+            days.push(key);
+        }
+    }
+
+    days.sort();
+
+    this.distribute = function() {
+        current += 1;
+        if (current == days.length) {
+            current = 0;
+        }
+
+        var current_points_idx = point_dates_obj[days[current]];
+        event_box.innerHTML = current_points_idx.display_format;
+        for (i = 0; i < num_points; i++) {
+            if (current_points_idx.includes(i)) {
+                points[i].style.opacity = 1;
+                points[i].style.visibility = 'visible';
+            } else {
+                points[i].style.opacity = 0;
+                points[i].style.visibility = 'hidden';
+            }
+        } rotate_hand_to(points[current_points_idx[0]]);
+    };
+
+    this.current_points = function() {      // FIXME: could be an iterator, or the values could probably be stored
+        var cur_points = [];
+        var current_points_idx = point_dates_obj[days[current]];
+        for (i = 0; i < num_points; i++) {
+            if (current_points_idx.includes(i)) {
+                cur_points.push(points[i]);
+            }
+        } return cur_points;
+    }
+
+    this.distribute();
 }
 
 function get_time_from_date(date) {
@@ -27,32 +104,10 @@ function rotate_hand_to(point) {
     document.getElementById('hand').style.transform = 'rotate(' + point.angle + 'deg)';
 }
 
-function distribute_points() {      // assumes that all events begin & end in 12 hours!
-    var points = document.querySelectorAll('.point');
-    var num_points = points.length;
-    var event_times = document.querySelectorAll('.event-time');
-    var event_titles = document.querySelectorAll('.event-title');
-
-    if (num_points != event_times.length || num_points != event_titles.length) {
-        throw "Exception: Unequal numbers of events & times! Check 'schedule.html'!"
-    }
-
-    for (var i = 0; i < num_points; i++) {
-        var date = new Date(event_times[i].firstChild.nodeValue);
-        var angle = (date.getHours() - 3) * 30 + date.getMinutes() / 2;
-        var x = 200 * Math.cos(Math.PI * angle / 180);
-        var y = 200 * Math.sin(Math.PI * angle / 180);
-        points[i].style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-        points[i].angle = angle;
-        points[i].event_time = get_time_from_date(date);
-        points[i].event_title = event_titles[i].firstChild.nodeValue;
-    } rotate_hand_to(points[0]);
-}
-
 function move_hand_to(e) {
     window.clock_timer.pause();
     var hand = document.getElementById('hand');
-    var points = document.querySelectorAll('.point');
+    var points = window.distributor.current_points();
 
     for (var i = 0; i < points.length; i++) {
         if (points[i] == e) {
@@ -64,15 +119,16 @@ function move_hand_to(e) {
 }
 
 window.onload = function() {
-    distribute_points();
-    var points = document.querySelectorAll('.point');
+    window.distributor = new EventDistributor();
+    var points = window.distributor.current_points();
     var hand = document.getElementById('hand');
     hand.idx = 1;
     window.clock_timer = new Timer(function() {       // put it inside the window so that we can access later
-        rotate_hand_to(points[hand.idx]);
-        hand.idx += 1;
         if (hand.idx == points.length) {
+            window.distributor.distribute();
+            points = window.distributor.current_points();
             hand.idx = 0;
-        }
+        } rotate_hand_to(points[hand.idx]);
+        hand.idx += 1;
     }, 2500);
 }
