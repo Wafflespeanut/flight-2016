c66155e90d53fa981e76a5110c9dc16852f124d9
Ravi Shankar <wafflespeanut@gmail.com>
2016-02-07 10:43:45 +0530

    Handing over some weight-lifting to JS, and mMaking some of the fonts static

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,6 +1,9 @@
+const CARDS_PER_STACK = 5;
 const IMAGE_WIDTH = 50;
+const MAX_WIDTH = 700;
 const SCROLL_WIDTH = 150;
-const TRANS_HEIGHT = 300;
+const TITLE_TRANS_HEIGHT = 30;
+const DESC_TRANS_HEIGHT = 300;
 const RIGHT = '0deg';
 const LEFT = '180deg';
 
@@ -13,11 +16,11 @@ function card_open(e) {
 
     var title = e.querySelector('.card-title');
     title.style.fontSize = '15px';
-    title.style.transform = 'translateY(' + TRANS_HEIGHT + '%)';
+    title.style.transform = 'translateY(' + TITLE_TRANS_HEIGHT + '%)';
     var description = e.querySelector('.card-description');
     description.style.opacity = 1;
     description.style.visibility = 'visible';
-    description.style.transform = 'translateY(' + (TRANS_HEIGHT / 2) + 'px)';
+    description.style.transform = 'translateY(' + (DESC_TRANS_HEIGHT / 2) + 'px)';
 }
 
 function card_close(e) {
@@ -31,33 +34,50 @@ function card_close(e) {
     title.style.fontSize = '20px';
     title.style.transform = 'translateY(0px)';
     var description = e.querySelector('.card-description');
-    description.style.transform = 'translateY(' + TRANS_HEIGHT + 'px)';
+    description.style.transform = 'translateY(' + DESC_TRANS_HEIGHT + 'px)';
     description.style.opacity = 0;
     description.style.visibility = 'hidden';
 }
 
 function Scroller() {
-    var pos = 0;
-    var scrollarea = document.getElementById('scroll-holder');
-    var stacks = document.querySelectorAll('.stack');
-    stacks[0].style.transform = 'translateX(0px)';
+    var pos = 0, stack, left_arrow, right_arrow;
+    var scroll_area = document.getElementById('scroll-holder');
+    var cards = document.querySelectorAll('.card');
     document.addEventListener('keydown', scroll, false);
+    var num_cards = cards.length;
 
-    var left_arrow = document.getElementById('arrow-left');
-    if (left_arrow != undefined) {
+    for (i = 0; i < num_cards; i++) {
+        if (i % CARDS_PER_STACK == 0) {
+            stack = document.createElement('div');
+            stack.className = 'stack';
+            scroll_area.appendChild(stack);
+        } stack.appendChild(cards[i]);
+    }
+
+    if (num_cards > CARDS_PER_STACK) {
+        left_arrow = document.createElement('img');
+        left_arrow.className = 'arrow';
+        left_arrow.id = 'arrow-left';
+        left_arrow.src = 'images/side-arrow.png';
         left_arrow.addEventListener('mouseup', left_up, false);
         left_arrow.addEventListener('mousedown', left_down, false);
-    }
+        scroll_area.appendChild(left_arrow);
 
-    var right_arrow = document.getElementById('arrow-right');
-    if (right_arrow != undefined) {
+        right_arrow = document.createElement('img');
+        right_arrow.className = 'arrow';
+        right_arrow.id = 'arrow-right';
+        right_arrow.src = 'images/side-arrow.png';
         right_arrow.addEventListener('mouseup', right_up, false);
         right_arrow.addEventListener('mousedown', right_down, false);
+        scroll_area.appendChild(right_arrow);
     }
+
+    var stacks = document.querySelectorAll('.stack');
+    stacks[0].style.transform = 'translateX(0px)';
 
     function left_up() {
         left_arrow.style.transform = 'translate(' + 0 + 'px, ' + 0 + 'px) rotate(' + LEFT + ')';
-        previous_slide();
+        previous_stack();
     }
     function left_down() {
         left_arrow.style.transform = 'translate(' + 3 + 'px, ' + 3 + 'px) rotate(' + LEFT + ')';
@@ -65,14 +85,14 @@ function Scroller() {
 
     function right_up() {
         right_arrow.style.transform = 'translate(' + 0 + 'px, ' + 0 + 'px) rotate(' + RIGHT + ')';
-        next_slide();
+        next_stack();
     }
 
     function right_down() {
         right_arrow.style.transform = 'translate(' + 3 + 'px, ' + 3 + 'px) rotate(' + RIGHT + ')';
     }
 
-    function next_slide() {
+    function next_stack() {
         if (pos < stacks.length - 1) {
             stacks[pos].style.transform = 'translateX(-' + SCROLL_WIDTH + '%)';
             pos += 1;
@@ -84,7 +104,7 @@ function Scroller() {
         }
     }
 
-    function previous_slide() {
+    function previous_stack() {
         if (pos > 0) {
             stacks[pos].style.transform = 'translateX(' + SCROLL_WIDTH + '%)';
             pos -= 1;
@@ -100,14 +120,14 @@ function Scroller() {
         var key_code = e.keyCode;
         if (key_code == 37) {
             e.preventDefault();
-            previous_slide();
+            previous_stack();
         } else if (key_code == 39) {
             e.preventDefault();
-            next_slide();
+            next_stack();
         } else if (key_code == 38 || key_code == 33 || key_code == 36 ||
                   (key_code == 32 && e.shiftKey) || key_code == 40 ||
                   key_code == 34 || key_code == 32 || key_code == 35) {
-            e.preventDefault();
+            e.preventDefault();         // arrest the other keys!
         }
     }
 }
