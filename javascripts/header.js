6f2d1a7b206fd56120159486c8fdc4daf7445be1
Ravi Shankar <wafflespeanut@gmail.com>
2016-02-13 23:50:10 +0530

    Workaround for responsive images

--- javascripts/header.js
+++ javascripts/header.js
@@ -2,9 +2,9 @@ const LIST_ITEM_TIMEOUT = 200;
 const MAX_WIDTH = 700;
 // probably ridiculous way of doing this, say whatever you want, I'm very lazy...
 const HEADER = '<div id="mobile-header"> \
-    <img id="mobile-logo" src="images/flight-white.png" /> \
+    <img id="mobile-logo" data-desktop="images/flight-white.png" data-mobile="images-mobile/flight-white.png" /> \
     <div id="header-text">Flight \'16</div> \
-    <img id="header-button" src="images/propeller.png" /> \
+    <img id="header-button" data-desktop="images/propeller.png" data-mobile="images-mobile/propeller.png" /> \
 </div> \
 <div id="header"> \
     <div id="mobile-bg" class="bg-image"> \
@@ -12,9 +12,9 @@ const HEADER = '<div id="mobile-header"> \
     </div> \
     <div class="tab"> \
         <a href="future.html"> \
-            <img id="home-logo" style="width: 150px" src="images/flight-white.png" /> \
+            <img id="home-logo" style="width: 150px" data-desktop="images/flight-white.png" data-mobile="images-mobile/flight-white.png" /> \
             <div id="home-tab" class="tab-content"> \
-                <img src="images/home.png" /> \
+                <img data-desktop="images/home.png" data-mobile="images-mobile/home.png" /> \
                 <span class="tab-text">Home</span> \
             </div> \
         </a> \
@@ -22,7 +22,7 @@ const HEADER = '<div id="mobile-header"> \
     <div class="tab"> \
         <a href="events.html"> \
             <div class="tab-content"> \
-                <img src="images/events.png" /> \
+                <img data-desktop="images/events.png" data-mobile="images-mobile/events.png" /> \
                 <span class="tab-text">Events</span> \
             </div> \
         </a> \
@@ -30,7 +30,7 @@ const HEADER = '<div id="mobile-header"> \
     <div class="tab"> \
         <a href="workshops.html"> \
             <div class="tab-content"> \
-                <img src="images/workshop.png" /> \
+                <img data-desktop="images/workshop.png" data-mobile="images-mobile/workshop.png" /> \
                 <span class="tab-text">Workshops</span> \
             </div> \
         </a> \
@@ -38,7 +38,7 @@ const HEADER = '<div id="mobile-header"> \
     <div class="tab"> \
         <a href="contact.html"> \
             <div class="tab-content"> \
-                <img src="images/phone.png" /> \
+                <img data-desktop="images/phone.png" data-mobile="images-mobile/phone.png" /> \
                 <span class="tab-text">Contact us</span> \
             </div> \
         </a> \
@@ -89,5 +89,31 @@ function transit_list(query_select) {
     var event_list = document.querySelectorAll(query_select);
     for (i = 0; i < event_list.length; i++) {
         show_elem(event_list[i], (i + 1) * LIST_ITEM_TIMEOUT);
+    }
+}
+
+function switch_images() {
+    var images = document.getElementsByTagName('img');
+    if (window.innerWidth <= MAX_WIDTH) {
+        for (i = 0; i < images.length; i++) {
+            images[i].src = images[i].dataset.mobile;
+        }
+    } else {
+        for (i = 0; i < images.length; i++) {
+            images[i].src = images[i].dataset.desktop;
+        }
+    }
+
+    var bg_images = document.querySelectorAll('.bg-marker');
+    if (bg_images) {
+        if (window.innerWidth <= MAX_WIDTH) {
+            for (i = 0; i < bg_images.length; i++) {
+                bg_images[i].style.backgroundImage = 'url(' + bg_images[i].dataset.mobile + ')';
+            }
+        } else {
+            for (i = 0; i < bg_images.length; i++) {
+                bg_images[i].style.backgroundImage = 'url(' + bg_images[i].dataset.desktop + ')';
+            }
+        }
     }
 }
